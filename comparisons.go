3799f36de9529484c704727d0c25c53accb85db2
John Muth <johnmuthuk@gmail.com>
2015-05-15 21:45:11 +0100

    more tests & doc

--- comparisons.go
+++ comparisons.go
@@ -1,3 +1,4 @@
+// Package xmlcompare provides functions for comparing bits of XML.
 package xmlcompare
 
 import (
@@ -7,7 +8,9 @@ import (
 	"github.com/clbanning/mxj"
 )
 
-// IsCompatible checks that two XML strings are structurally the same so that they are compatible. The first string should be your "correct" XML, if there are extra elements in B then they will still be seen as compatible.
+// IsCompatible compares two XML strings and returns true if the second has all the same element names and value types as the first.
+// The elements don't have to be in the same order.
+// The second XML string can have additional elements not present in the first.
 func IsCompatible(a, b string) (compatible bool, err error) {
 	aMap, err := mxj.NewMapXml([]byte(a), true)
 	if err != nil {
@@ -33,12 +36,9 @@ func isStructurallyTheSame(a, b map[string]interface{}) (compatible bool, err er
 				}
 				compatible = true
 			}
-			return
 		default:
 			err = fmt.Errorf("Unmatched datatype in XML found, got a %v", reflect.TypeOf(v))
-			return
 		}
 	}
-	compatible = true
 	return
 }
--- comparisons_test.go
+++ comparisons_test.go
@@ -6,8 +6,10 @@ import (
 
 const baseXML = `<human><firstname>chris</firstname><lastname>james</lastname><age>30</age></human>`
 const compatibleXML = `<human><firstname>christopher</firstname><lastname>james</lastname><age>15</age></human>`
+const differentOrderXML = `<human><age>30</age><firstname>chris</firstname><lastname>james</lastname></human>`
 const differentElementNamesXML = `<wildebeest><name>Blue</name></wildebeest>`
 const differentValueTypeXML = `<human><firstname>kristofferson</firstname><lastname>james</lastname><age>old</age></human>`
+const invalidXML = `not even xml`
 
 func TestIdenticalXMLIsCompatible(t *testing.T) {
 	assertCompatible(t, baseXML, baseXML)
@@ -17,12 +19,24 @@ func TestXMLWithSameElementNamesAndValueTypesIsCompatible(t *testing.T) {
 	assertCompatible(t, baseXML, compatibleXML)
 }
 
+func TestXMLWithSameElementsInDifferentOrderIsCompatible(t *testing.T) {
+	assertCompatible(t, baseXML, differentOrderXML)
+}
+
 func TestXMLWithDifferentElementNamesIsIncompatible(t *testing.T) {
 	assertIncompatible(t, baseXML, differentElementNamesXML)
 }
 
 func TestXMLWithDifferentValueTypesIsIncompatible(t *testing.T) {
 	assertIncompatible(t, baseXML, differentValueTypeXML)
+}
+
+func TestFirstInvalidXMLIsIncompatibleAndReturnsError(t *testing.T) {
+	assertIncompatibleAndError(t, invalidXML, baseXML)
+}
+
+func TestSecondInvalidXMLIsIncompatibleAndReturnsError(t *testing.T) {
+	assertIncompatibleAndError(t, baseXML, invalidXML)
 }
 
 func assertCompatible(t *testing.T, a, b string) {
@@ -34,5 +48,11 @@ func assertCompatible(t *testing.T, a, b string) {
 func assertIncompatible(t *testing.T, a, b string) {
 	if compatible, err := IsCompatible(a, b); compatible || err != nil {
 		t.Errorf("%s should not be compatible with %s (err = %v)", a, b, err)
+	}
+}
+
+func assertIncompatibleAndError(t *testing.T, a, b string) {
+	if compatible, err := IsCompatible(a, b); compatible || err == nil {
+		t.Errorf("%s should not be compatible with %s and it should return an error", a, b)
 	}
 }
